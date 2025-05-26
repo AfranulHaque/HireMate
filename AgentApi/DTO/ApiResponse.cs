343d3a6075e0308fc4308894bb851f257401d2e9
md-imran97 <mdimran.ml109@gmail.com>
2025-05-27 02:40:00 +0600

    Added new api project to request LLM

---
+++ AgentApi/DTO/ApiResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace AgentApi.DTO
+{
+    public class ApiResponse<T>
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public T data { get; set; }
+    }
+}
---
+++ AgentApi/DTO/CandidateCheckRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace AgentApi.DTO
+{
+    public class CandidateCheckRequest
+    {
+        public string JobDescription { get; set; }
+        public string CandidateInformation { get; set; }
+    }
+}
