89b8d358dcfe59267202f3d1a81251b9e709490a
md-imran97 <62457979+md-imran97@users.noreply.github.com>
2025-05-28 07:15:48 +0600

    Added chat functionality

--- HireMate/Program.cs
+++ HireMate/Program.cs
@@ -8,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddSession();
+
 builder.Services.AddExternalCollections();
 builder.Services.AddServiceCollection();
 builder.Services.AddConfigCollections(builder.Configuration);
@@ -24,7 +26,7 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
+app.UseSession();
 app.UseRouting();
 
 app.UseAuthorization();
